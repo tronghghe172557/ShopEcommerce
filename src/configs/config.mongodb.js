7ddb08f1047bcc15a7d88416a0fea0d369267d6c
Tronghghe172557 <tronghghe172557@fpt.edu.vn>
2024-05-18 23:33:50 +0700

    feat: Người ở level 3 viết configs cho DB như này đây

--- src/configs/config.mongodb.js
+++ src/configs/config.mongodb.js
@@ -1,15 +1,42 @@
 'use strict'
 
 // LV 0
-const config = {
+// const config = {
+//     app: {
+//         port: 3000
+//     },
+//     db: {
+//         host: 'localhost',
+//         port: 27017,
+//         name: 'shopDEV'
+//     }
+// }
+
+// LEVEL 01
+const dev = {
     app: {
-        port: 3000
+        port: process.env.DEV_APP_PORT
     },
     db: {
-        host: 'localhost',
-        port: 27017,
-        name: 'shopDEV'
+        host: process.env.DEV_APP_HOST || 'localhost',
+        port: process.env.DEV_APP_PORT || 27017, 
+        name: process.env.DEV_APP_NAME || 'shopDEV'
     }
 }
 
-module.exports = config
+const pro = {
+    app: {
+        port: process.env.PRO_APP_PORT
+    },
+    db: {
+        host: process.env.PRO_DB_HOST || 'localhost',
+        port: process.env.PRO_DB_PORT || 27017,
+        name: process.env.PRO_DB_NAME || 'dbpro'
+    }
+}
+
+const config = {dev, pro}; 
+const env = process.env.NODE_ENV || `dev`
+
+// console.log(config[env], env)
+module.exports = config[env]
