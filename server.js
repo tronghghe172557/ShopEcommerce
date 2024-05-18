9891ca60b510d012a0e79d5e18b63f3dce15c5a2
Tronghghe172557 <hoanggiatrang01Ggmail.com>
2024-05-18 20:35:00 +0700

    build: Fix lỗi cứ pending mãi Thiếu dấu () gọi hàm

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./src/app");
 
-const PORT = 3055
+const PORT = 3000;
 const server = app.listen(PORT, () => {
-    console.log(`WSW eCommerce start with ${PORT}`)
-})
+  console.log(`WSW eCommerce start with http://localhost:${PORT}`);
+});
 
-// khi cmd + c => tắt server và in ra màn hình
-process.on("SIGINT", () => {
-    server.close( () => console.log(`Exit Server Express`))
-    // app.notify.send(ping ...)
-})
+// // khi cmd + c => tắt server và in ra màn hình
+// process.on("SIGINT", () => {
+//   server.close(() => console.log(`Exit Server Express`));
+//   // app.notify.send(ping ...)
+// });
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const compression = require("compression");
 const express = require("express");
+const compression = require("compression");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 
@@ -7,12 +7,24 @@ const app = express();
 // init middlewares
 app.use(morgan("dev")); // lưu lại khi người dùng đăng nhập vào với status
 app.use(helmet()); // ngăn chặn người dùng lấy được thông tin web dùng được công nghệ gì
-app.use(compression); // nén dữ liệu để tiết kiệm băng thông
+app.use(compression()); // nén dữ liệu để tiết kiệm băng thông => lỗi ở đây :))
 
 // init db
 
 // init routes
+app.get("/", (req, res) => {
+  const strCompress = "Hello Trong";
 
-// handling error
+  return res.status(200).json({
+    message: `Wellcome Trong`,
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+// Handling error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 module.exports = app;
